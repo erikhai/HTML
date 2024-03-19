b1591b9c6daa72d0d0355cbc5b9157ce92752fc9
Erik Hai <erikh4558@gmail.com>
2024-03-19 11:49:06 +1100

    Re creating the website

---
+++ Version2/resume.js
@@ -0,0 +1,26 @@
+document.getElementById("resumeLink").addEventListener("click", function(event) {
+    // Prevent the default action (opening the link)
+    event.preventDefault();
+    
+    // Replace '#' with the actual URL of your resume file
+    var resumeUrl = "assets/resume.pdf";
+    
+    // Create a temporary link element
+    var link = document.createElement("a");
+    
+    // Set the href attribute to the URL of your resume file
+    link.href = resumeUrl;
+    
+    // Set the download attribute to force download the file
+    link.download = "resume.pdf";
+    
+    // Append the link to the document body
+    document.body.appendChild(link);
+    
+    // Trigger the click event on the link
+    link.click();
+    
+    // Remove the temporary link from the document body
+    document.body.removeChild(link);
+  });
+  
